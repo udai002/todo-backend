5e9b05e9bf1563e2766ae35cd3986205272cfce1
udai sai <karmuriudaisai002@gmail.com>
2024-02-26 23:56:31 +0530

    mongodb removed

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ app = express()
 
 console.log(dotenv.parsed.PORT)
 
-try{
-    mongoose
-    .connect('mongodb+srv://karumuriudaisai002:udai123@cluster0.4o0q2x6.mongodb.net/')
-    .then(() => console.log('db connected'))
-    .catch(err => console.log(err))
-}catch(e){
-    console.log(e)
-}
+// try{
+//     mongoose
+//     .connect('mongodb+srv://karumuriudaisai002:udai123@cluster0.4o0q2x6.mongodb.net/')
+//     .then(() => console.log('db connected'))
+//     .catch(err => console.log(err))
+// }catch(e){
+//     console.log(e)
+// }
 
 
 app.use(express.json())
